568bc1638cf1ff2870bf0d5fc7fc6c3b6d0439d8
Niloy Khan <niloykhan.dev@gmail.com>
2026-05-27 23:10:00 +0600

    set up files and server

---
+++ backend/src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB.URI}`,
+    );
+    console.log(`/n MongoDB connected!!!
+      ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
